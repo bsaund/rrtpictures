ed8b94da0197a5b8db38c586ffdafdee22267a28
Brad Saund <brad.saund@gmail.com>
2019-07-29 10:24:16 -0400

    Added lsc, which seems to work better than slic

--- painting_utils.py
+++ painting_utils.py
@@ -88,17 +88,7 @@ def display_until(canvas, name="tmp"):
         pass
 
 
-def make_slic(img, region_size=int(10), ruler=10.0):
-    sp = cv2.ximgproc.createSuperpixelSLIC(img, region_size=region_size, ruler=ruler)
-    for _ in range(5):
-        sp.iterate()
-    return sp
-
-
-def disp_slic(img, region_size=10, ruler=10.0):
-    sp = make_slic(img, region_size, ruler)
-    con = sp.getLabelContourMask()
-
+def disp_contour(img, con):
     di = cv2.dilate(con, np.ones((3,3), np.uint8), iterations=1)
 
     white = np.ones(img.shape, np.uint8)*255
@@ -107,3 +97,37 @@ def disp_slic(img, region_size=10, ruler=10.0):
     mix = cv2.bitwise_and(mix, mix, mask=cv2.bitwise_not(con))
     
     display_until(mix)
+    
+    
+def make_slic(img, region_size=int(10), ruler=10.0):
+    """
+    SLIC
+    https://docs.opencv.org/3.4/df/d6c/group__ximgproc__superpixel.html
+    """
+        
+    sp = cv2.ximgproc.createSuperpixelSLIC(img, region_size=region_size, ruler=ruler)
+    for _ in range(5):
+        sp.iterate()
+    return sp
+
+def disp_slic(img, region_size=10, ruler=10.0):
+    sp = make_slic(img, region_size, ruler)
+    con = sp.getLabelContourMask()
+
+    disp_contour(img, con)
+
+def make_lsc(img, region_size=10, ratio=0.075):
+    """
+    NOTE: This works better than slic it seems
+
+    Linear Spectral Clustering
+    https://docs.opencv.org/3.4/df/d6c/group__ximgproc__superpixel.html
+    """
+    sp = cv2.ximgproc.createSuperpixelLSC(img, region_size=region_size, ratio=ratio)
+    for _ in range(5):
+        sp.iterate()
+    return sp
+
+def disp_lsc(img, region_size=10, ratio=0.075):
+    sp = make_lsc(img, region_size, ratio)
+    disp_contour(img, sp.getLabelContourMask())
